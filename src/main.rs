bd4aa3f4503e4f46d89fa51ae0946c43b5a13c80
williambrach <wibrach@gmail.com>
2024-11-07 11:29:50 +0100

    fix in qr code view box

--- src/main.rs
+++ src/main.rs
@@ -306,7 +306,7 @@ fn app() -> Html {
                                     html! {
                                         <div>
                                         <div class="qr-code">
-                                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 30 30" stroke="none">
+                                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 33 33" stroke="none">
                                         {
                                             if *transparent_background {
                                                 html! {}
